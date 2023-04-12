41e9fdefcfd848b02e44359e8de2f554f0a59bb8
JordiSoler99 <130551023+JordiSoler99@users.noreply.github.com>
2023-04-12 11:14:22 +0200

    Add files via upload

---
+++ getin.allfunds.com/config.js
@@ -0,0 +1,8 @@
+window.appConfig = {
+  API_URL: "https://onboarding.allfunds.com/api",
+  HTTP_TOKEN: "GLhYEuzVUrkos3d8je5VmVrtL3z89gbuCYdZkgcAJqO",
+  ESIGN_RETURNURL: "https://onboarding.allfunds.com/signed-success",
+  DASHBOARD_URL: "https://dashboard.allfunds.com",
+  HOTJAR_KEY: "1277762",
+  HOTJAR_LABEL: "6"
+};
---
+++ getin.allfunds.com/static/js/8.f74709d9b0c4b25205e6.js
@@ -0,0 +1,1 @@
+window.location.replace("https://getin.allfunds.com/login");

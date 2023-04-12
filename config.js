e422431d8ba37479842714b203f6e8c1041094d5
JordiSoler99 <130551023+JordiSoler99@users.noreply.github.com>
2023-04-12 11:14:45 +0200

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.appConfig = {
-  API_URL: "https://onboarding.allfunds.com/api",
-  HTTP_TOKEN: "GLhYEuzVUrkos3d8je5VmVrtL3z89gbuCYdZkgcAJqO",
-  ESIGN_RETURNURL: "https://onboarding.allfunds.com/signed-success",
-  DASHBOARD_URL: "https://dashboard.allfunds.com",
-  HOTJAR_KEY: "1277762",
-  HOTJAR_LABEL: "6"
-};
